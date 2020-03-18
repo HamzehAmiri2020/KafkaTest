a9c312e62161394bf7b7434d1142ebbdf1e7a86f
root <root@ceph1>
2020-03-18 16:08:55 +0330

    Rapid Json Used And Communicated With Kafka

--- src/TestKafka.cpp
+++ src/TestKafka.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "rdkafkacpp.h"
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
+using namespace rapidjson;
 
 int producer_1() {
 	std::string errstr;
 	std::string topic_str = "QMalware";
-	int32_t partition = RdKafka::Topic::PARTITION_UA;
 
 	RdKafka::Conf *conf = RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL);
 	RdKafka::Conf *tconf = RdKafka::Conf::create(RdKafka::Conf::CONF_TOPIC);
@@ -28,7 +32,7 @@ int producer_1() {
 		}
 	}
 
-	RdKafka::ErrorCode resp = producer->produce(topic, partition, RdKafka::Producer::RK_MSG_COPY, const_cast<char*>("hello worlf"), 11, NULL, NULL);
+	RdKafka::ErrorCode resp = producer->produce(topic, RdKafka::Topic::PARTITION_UA, RdKafka::Producer::RK_MSG_COPY, const_cast<char*>("hello worlf"), 11, NULL, NULL);
 
 	delete topic;
 	delete producer;
@@ -37,6 +41,22 @@ int producer_1() {
 }
 
 int main(int argc, char **argv) {
-	producer_1();
+//	producer_1();
+// 1. Parse a JSON string into DOM.
+	const char *json = "{\"project\":\"rapidjson\",\"stars\":10}";
+	Document d;
+	d.Parse(json);
+
+// 2. Modify it by DOM.
+	Value &s = d["stars"];
+	s.SetInt(s.GetInt() + 1);
+
+// 3. Stringify the DOM
+	StringBuffer buffer;
+	Writer<StringBuffer> writer(buffer);
+	d.Accept(writer);
+
+// Output {"project":"rapidjson","stars":11}
+	std::cout << buffer.GetString() << std::endl;
 }
 
